9e62f23c0340e4a82cb075ad26f45988e9d9c700
Sekhar Shrestha <95266964+SekharShrestha@users.noreply.github.com>
2022-11-21 10:53:18 +0530

    Revert "Userserv implementation"

--- Userserv/src/main/java/com/userserv/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.userserv.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.userserv.entities.User;
-
-public interface UserRepo extends JpaRepository<User, String> {
-
-}
